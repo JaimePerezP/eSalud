a8387a25aedec7aef8981d46b92a2a7152888241
JaimePerezP <43539902+JaimePerezP@users.noreply.github.com>
2019-11-28 14:04:43 +0100

    1.0.1
    Fix ruta de features en TestRunner

--- src/test/java/es/e3corp/eSalud/bdd/TestRunner.java
+++ src/test/java/es/e3corp/eSalud/bdd/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/featuresModificar")
+@CucumberOptions(features = "src/test/resources/featuresMantenimiento")
 public class TestRunner {
 
 }
