d68f75530e0ebe848f48a244bdc858e706f7077e
JaimePerezP <43539902+JaimePerezP@users.noreply.github.com>
2019-11-28 13:55:13 +0100

    1.0.1
    Cambio de carpeta de pruebas

--- src/test/java/es/e3corp/eSalud/bdd/TestRunner.java
+++ src/test/java/es/e3corp/eSalud/bdd/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/resources/features")
+@CucumberOptions(features = "src/test/resources/featuresModificar")
 public class TestRunner {
 
 }
