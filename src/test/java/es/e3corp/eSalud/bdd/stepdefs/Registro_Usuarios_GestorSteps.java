ecb736016f73d2a93cc43ae8dc862183e848fdd0
JaimePerezP <43539902+JaimePerezP@users.noreply.github.com>
2019-12-02 14:18:34 +0100

    definición de pasos

---
+++ src/test/java/es/e3corp/eSalud/bdd/stepdefs/Registro_Usuarios_GestorSteps.java
@@ -0,0 +1,57 @@
+package es.e3corp.eSalud.bdd.stepdefs;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Registro_Usuarios_GestorSteps {
+
+	@Given("se registra un gestor")
+	public void se_registra_un_gestor(io.cucumber.datatable.DataTable dataTable) {
+		// Write code here that turns the phrase above into concrete actions
+		// For automatic transformation, change DataTable to one of
+		// E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+		// Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+		// Double, Byte, Short, Long, BigInteger or BigDecimal.
+		//
+		// For other transformations you can register a DataTableType.
+		throw new cucumber.api.PendingException();
+	}
+
+	@Given("se abre navegador y se va a pantalla de inicio")
+	public void se_abre_navegador_y_se_va_a_pantalla_de_inicio() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@Given("se introducen los datos de inicio de sesion del gestor")
+	public void se_introducen_los_datos_de_inicio_de_sesion_del_gestor(io.cucumber.datatable.DataTable dataTable) {
+		// Write code here that turns the phrase above into concrete actions
+		// For automatic transformation, change DataTable to one of
+		// E, List<E>, List<List<E>>, List<Map<K,V>>, Map<K,V> or
+		// Map<K, List<V>>. E,K,V must be a String, Integer, Float,
+		// Double, Byte, Short, Long, BigInteger or BigDecimal.
+		//
+		// For other transformations you can register a DataTableType.
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("entra en la vista del gestor")
+	public void entra_en_la_vista_del_gestor() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@When("se va a la ventana de registro del gestor")
+	public void se_va_a_la_ventana_de_registro_del_gestor() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+	@Then("el gestor lo registra en la aplicacion")
+	public void el_gestor_lo_registra_en_la_aplicacion() {
+		// Write code here that turns the phrase above into concrete actions
+		throw new cucumber.api.PendingException();
+	}
+
+}
---
+++ src/test/resources/featuresMantenimiento/Registro_Usuarios_Gestor.feature
@@ -0,0 +1,26 @@
+Feature: El gestor puede registrar usuarios
+
+	Scenario Outline: <testCase> <resultadoEsperado>
+	
+	Given se registra un gestor
+	|	 dni	   | nombre	  	| apellidos      | pwd 					| numTelefono    | email             |  localidad   | rol 	 |
+  | 00000002 | Prueba     | Prueba Bien    | Prueba-12345	| 645612514      | prueba@prueba.com |	Ciudad real	| Admin  |
+	
+	Given se abre navegador y se va a pantalla de inicio
+	
+	And se introducen los datos de inicio de sesion del gestor
+		|	dni	    | pwd  				|  
+    | 00000002| Prueba-12345|
+    
+  When entra en la vista del gestor
+  
+  And se va a la ventana de registro del gestor
+  
+  Then el gestor lo registra en la aplicacion
+  |	 dni	| nombre	 | apellidos   | pwd 	 | numTelefono    | email   |  localidad  | rol 	 |
+  | <dni> | <nombre> | <apellidos> | <pwd> | <numTelefono>  | <email> |	<localidad>	| <rol>  |
+	
+	Examples: 
+  	|testCase	| resultadoEsperado 			 |	 dni	  | nombre	  	| apellidos      | pwd 					| pwd2  				| numTelefono    | email             |  localidad   | rol 	 | especialidad |
+    |CASO 1		| REGISTRO OK				 			 | 00000001	| Prueba      | Prueba Bien    | Prueba-12345	| Prueba-12345  | 645612514      | prueba@prueba.com |	Ciudad real	| medico | Cabecera     |
+    |CASO 2		| FALLO, DNI EXISTENTE		 | 00000001	| Prueba      | Prueba Bien    | Prueba-12345	| Prueba-12345  | 645612514      | prueba@prueba.com |	Ciudad real	| medico | Cabecera			|
