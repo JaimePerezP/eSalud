7cce961487e3fbe5c467f83e28bdc2c102fdf6e3
JaimePerezP <43539902+JaimePerezP@users.noreply.github.com>
2019-12-03 13:33:20 +0100

    Relleno de codigo de pruebas

--- src/test/java/es/e3corp/eSalud/bdd/stepdefs/Registro_Usuarios_GestorSteps.java
+++ src/test/java/es/e3corp/eSalud/bdd/stepdefs/Registro_Usuarios_GestorSteps.java
@@ -1,10 +1,26 @@
 package es.e3corp.eSalud.bdd.stepdefs;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import es.e3corp.eSalud.Service.UsuarioService;
+import es.e3corp.eSalud.model.Usuario;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class Registro_Usuarios_GestorSteps {
+
+	List<Map<String, String>> tabla;
+	ChromeDriver driver = WebDriver.webDriver;
+	UsuarioService usersService;
 
 	@Given("se registra un gestor")
 	public void se_registra_un_gestor(io.cucumber.datatable.DataTable dataTable) {
@@ -15,13 +31,22 @@ public class Registro_Usuarios_GestorSteps {
 		// Double, Byte, Short, Long, BigInteger or BigDecimal.
 		//
 		// For other transformations you can register a DataTableType.
-		throw new cucumber.api.PendingException();
+		tabla = dataTable.asMaps(String.class, String.class);
+		String dni = tabla.get(0).get("dni");
+		String nombre = tabla.get(0).get("nombre");
+		String apellidos = tabla.get(0).get("apellidos");
+		String pwd = tabla.get(0).get("pwd");
+		String tlf = tabla.get(0).get("numTelefono");
+		String email = tabla.get(0).get("email");
+		String localidad = tabla.get(0).get("localidad");
+		String rol = tabla.get(0).get("rol");
+		Usuario usuario = new Usuario(dni, nombre, apellidos, pwd, rol, null, null, tlf, localidad, null, email);
+		usersService.saveUsuario(usuario);
 	}
 
 	@Given("se abre navegador y se va a pantalla de inicio")
 	public void se_abre_navegador_y_se_va_a_pantalla_de_inicio() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new cucumber.api.PendingException();
+		driver.get("http://localhost:8080/auth/login");
 	}
 
 	@Given("se introducen los datos de inicio de sesion del gestor")
@@ -33,25 +58,32 @@ public class Registro_Usuarios_GestorSteps {
 		// Double, Byte, Short, Long, BigInteger or BigDecimal.
 		//
 		// For other transformations you can register a DataTableType.
-		throw new cucumber.api.PendingException();
+		driver.findElement(By.xpath("//*[@id=\"dni\"]")).sendKeys(tabla.get(0).get("dni"));
+		driver.findElement(By.xpath("//*[@id=\"password\"]")).sendKeys(tabla.get(0).get("pwd"));
+		driver.findElement(By.xpath("/html/body/linl/app-root/app-login/div/form/div/div/p[1]/input")).click();
 	}
 
 	@When("entra en la vista del gestor")
 	public void entra_en_la_vista_del_gestor() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new cucumber.api.PendingException();
+		WebDriverWait wait = new WebDriverWait(driver, 2);
+		wait.until(ExpectedConditions.visibilityOf(
+				driver.findElementByXPath("/html/body/linl/app-root/app-sidenavadmin/div/mat-toolbar/h1")));
+		assertEquals(
+				driver.findElementByXPath("/html/body/linl/app-root/app-sidenavadmin/div/mat-toolbar/h1").getText(),
+				"Administrador");
 	}
 
 	@When("se va a la ventana de registro del gestor")
 	public void se_va_a_la_ventana_de_registro_del_gestor() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new cucumber.api.PendingException();
+		if (tabla.get(0).get("resultadoEsperado") == "REGISTRO MEDICO OK") {
+			driver.findElementByXPath(
+					"/html/body/linl/app-root/app-sidenavadmin/div/mat-sidenav-container/mat-sidenav/div/mat-nav-list/a[3]/div/div[1]").click();
+		}
 	}
 
 	@Then("el gestor lo registra en la aplicacion")
 	public void el_gestor_lo_registra_en_la_aplicacion() {
-		// Write code here that turns the phrase above into concrete actions
-		throw new cucumber.api.PendingException();
+
 	}
 
 }
--- src/test/resources/featuresMantenimiento/Registro_Usuarios_Gestor.feature
+++ src/test/resources/featuresMantenimiento/Registro_Usuarios_Gestor.feature
@@ -4,7 +4,7 @@ Feature: El gestor puede registrar usuarios
 	
 	Given se registra un gestor
 	|	 dni	   | nombre	  	| apellidos      | pwd 					| numTelefono    | email             |  localidad   | rol 	 |
-  | 00000002 | Prueba     | Prueba Bien    | Prueba-12345	| 645612514      | prueba@prueba.com |	Ciudad real	| Admin  |
+  | 00000002 | Prueba     | Prueba Bien    | Prueba-12345	| 645612514      | prueba@prueba.com |	Ciudad real	| admin  |
 	
 	Given se abre navegador y se va a pantalla de inicio
 	
@@ -21,6 +21,8 @@ Feature: El gestor puede registrar usuarios
   | <dni> | <nombre> | <apellidos> | <pwd> | <numTelefono>  | <email> |	<localidad>	| <rol>  |
 	
 	Examples: 
-  	|testCase	| resultadoEsperado 			 |	 dni	  | nombre	  	| apellidos      | pwd 					| pwd2  				| numTelefono    | email             |  localidad   | rol 	 | especialidad |
-    |CASO 1		| REGISTRO OK				 			 | 00000001	| Prueba      | Prueba Bien    | Prueba-12345	| Prueba-12345  | 645612514      | prueba@prueba.com |	Ciudad real	| medico | Cabecera     |
-    |CASO 2		| FALLO, DNI EXISTENTE		 | 00000001	| Prueba      | Prueba Bien    | Prueba-12345	| Prueba-12345  | 645612514      | prueba@prueba.com |	Ciudad real	| medico | Cabecera			|
+  	|testCase	| resultadoEsperado 			 |	 dni	  | nombre	  	| apellidos      | pwd 					| pwd2  				| numTelefono    | email             |  localidad   | rol 	   | especialidad |
+    |CASO 1		| REGISTRO MEDICO OK			 | 00000001	| Prueba      | Prueba Bien    | Prueba-12345	| Prueba-12345  | 645612514      | prueba@prueba.com |	Ciudad real	| medico   | Cabecera     |
+    |CASO 2		| REGISTRO PACIENTE OK		 | 00000003	| Prueba      | Prueba Bien    | Prueba-12345	| Prueba-12345  | 645612514      | prueba@prueba.com |	Ciudad real	| paciente | Cabecera			|
+    |CASO 3		| REGISTRO ADMIN OK		 		 | 00000004	| Prueba      | Prueba Bien    | Prueba-12345	| Prueba-12345  | 645612514      | prueba@prueba.com |	Ciudad real	| paciente | Cabecera			|
+    |CASO 4		| FALLO, DNI EXISTENTE		 | 00000001	| Prueba      | Prueba Bien    | Prueba-12345	| Prueba-12345  | 645612514      | prueba@prueba.com |	Ciudad real	| medico   | Cabecera			|
